0121bcbf2fed8f4866aaa47b8ec5d54d5737e27a
Crystal Schooling <bytesizemagic@gmail.com>
2020-04-01 09:53:49 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,11 +3,12 @@ class Dog
   
 
   attr_reader :name
-  attr_accessor :owner
+  attr_accessor :owner :mood
 
   def initialize(name, owner)
     @name = name
     @owner = owner
+    @mood = "nervous"
   end
 
 
